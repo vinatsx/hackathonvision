5973d8929d4fa102a228e90a32d46e3505f8b2e4
vinatsx <88601826+vinatsx@users.noreply.github.com>
2021-12-14 18:58:47 +0800

    Update and rename main.py to analyse.py

---
+++ analyse.py
@@ -0,0 +1,28 @@
+import requests
+from db import SUBSCRIPTION_KEY
+
+def analyseimg():
+
+  vision_service_address = "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/"
+  address = vision_service_address + "analyze"
+
+  parameters  = {'visualFeatures':'Objects','language':'en'}
+
+
+  img_path = f"./TestImages/crowd.jpg"
+  img_data = open(img_path, 'rb').read()
+
+  headers    = {'Content-Type': 'application/octet-stream',
+                'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY}
+
+  response = requests.post(address, headers=headers, params=parameters, data=img_data)
+
+  results = response.json()
+  counter = 0
+  for i in results['objects']:
+      if i['object'] == 'person':
+          counter+=1
+
+  print(counter)
+  
+  return counter
