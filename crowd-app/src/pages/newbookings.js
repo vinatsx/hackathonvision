c1e44d4fbb6e75e980e60bddc9d8f5bc20069147
vinatsx <e0775349@u.nus.edu>
2021-12-15 11:47:48 +0800

    navbar added

---
+++ crowd-app/src/pages/newbookings.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NewBooking = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh'
+      }}
+    >
+      <h1>Show Number</h1>
+    </div>
+  );
+};
+
+export default NewBooking;
